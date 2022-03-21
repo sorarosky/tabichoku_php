6102fc7e820c41e1cc5c80ecbe3983ed59422cfa
unknown-anna <unknown2011.04.art@google.com>
2022-03-21 00:33:29 -0700

    implement dashboard page

--- components/aside-mypage.php
+++ components/aside-mypage.php
@@ -1,3 +1,48 @@
 <aside class="aside">
-  aside
+  <div class="aside-box">
+    <div class="profile-portrait">
+      <img src="assets/img/mypage-portrait-no-image.svg" alt="">
+    </div>
+    <div class="profile-deial">
+      <p>会員IDが入る</p>
+      <p>名前　下の名前</p>
+      <p><a href="">アカウント情報の管理</a></p>
+    </div>
+  </div>
+
+  <div class="aside-box">
+    <ul class="aside-box-list">
+      <li class="aside-box-list-item">
+        <a href=""><img src="assets/img/mypage-nav-icon-profile.svg" alt="">会員情報</a>
+      </li>
+      <li class="aside-box-list-item">
+        <a href=""><img src="assets/img/mypage-nav-icon-favorite.svg" alt="">お気に入り</a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="aside-box">
+    <ul class="aside-box-list">
+        <li class="aside-box-list-item">
+          <a href=""><img src="assets/img/mypage-nav-icon-travel.svg" alt="">あなたの旅行情報</a>
+        </li>
+        <li class="aside-box-list-item">
+          <a href=""><img src="assets/img/mypage-nav-icon-people.svg" alt="">旅行者情報</a>
+        </li>
+        <li class="aside-box-list-item">
+          <a href=""><img src="assets/img/mypage-nav-icon-payment.svg" alt="">お支払い情報</a>
+        </li>
+        <li class="aside-box-list-item">
+          <a href=""><img src="assets/img/mypage-nav-icon-template.svg" alt="">テンプレート</a>
+        </li>
+      </ul>
+  </div>
+
+  <div class="aside-box aside-box-no-bg">
+    <a href="" class="banner">
+      <p class="banner-title">たびチョクに旅の相談</p>
+      <p class="banner-text">お問い合わせはお気軽に！</p>
+    </a>
+  </div>
+
 </aside>
--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,47 @@
 <?php include 'header-mypage.php';?>
 <?php include 'breadcrumb.php';?>
 <div class="contents-wrap mypage">
+
   <div class="main-aside-wrap">
+
     <main class="main dashboard">
-      main-mypage
+      <div class="main-header main-header-flex">
+        <h2 class="main-header-title">現在進行中のツアー</h2>
+        <div class=side><a href=""><button class="btn btn-blue-line">一覧を見る</button></a></div>
+      </div>
+
+      <ul class="main-list-tour">
+        <?php for($i=0;$i<3;$i++) { ?>
+        <li class="main-list-tour-item">
+          <div class="content">
+            <div class="box-flex">
+              <p class="box-flex-item"><span>受付日</span>0000/00/00</p>
+              <p class="box-flex-item"><span>ツアー番号</span>220001-001</p>
+            </div>
+            <h3 class="content-title">旅コンテンツタイトルが入る。タイトルが入るタイトルが入る。タイトルが入るタイトルが入る。タイトルが入る</h3>
+            <ul class="box-list">
+              <li class="box-list-item">
+                <p class="box-list-item-title">ツアー名</p>
+                <p class="box-list-item-text">木村ロスツアー</p>
+              </li>
+              <li class="box-list-item">
+                <p class="box-list-item-title">現地パートナー</p>
+                <p class="box-list-item-text">エレファントツアー</p>
+              </li>
+            </ul>
+          </div>
+          <div class="side">
+            <a href=""><button class="btn btn-blue">詳細をみる</button></a>
+          </div>
+        </li>
+        <?php } ?>
+
+      </ul>
+
     </main>
+
     <?php include 'components/aside-mypage.php';?>
   </div>
+
 </div>
 <?php include 'footer-mypage.php';?>
