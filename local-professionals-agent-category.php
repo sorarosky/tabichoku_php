c42dcb89180da21a4e326fdd848a92c75a879025
Sora <sora_tsukamoto@me.com>
2022-03-23 13:24:05 -0700

    commit 20220323

---
+++ local-professionals-agent-category.php
@@ -0,0 +1,39 @@
+<?php include 'header.php';?>
+<?php include 'components/travel-content-kv.php';?>
+<div class="contents-wrap">
+  <div class="main-aside-wrap">
+    <main class="main local-professionals-agent-category">
+      <section class="section-content posts">
+        <div class="inner">
+          <div class="head">日本（エリア）</div>
+          <div class="post">
+            <div class="img">
+ 
+            </div>
+            <div class="info">
+              <div class="title">エージェント名が入る</div>
+              <div class="copy">キャッチコピー（44文字）この文章はダミーです。文字の大きさ、量、字間、行間等12345</div>
+              <div class="country-name">イタリア</div>
+              <ul class="countries">
+                <li class="">取り扱い国名が入る</li>
+                <li class="">取り扱い国名が入る</li>
+                <li class="">取り扱い国名が入る</li>
+                <li class="">取り扱い国名が入る取り扱い国名が入る</li>
+              </ul>
+              <ul class="skills">
+                <li class="">得意分野が入る</li>
+                <li class="">得意分野が入る</li>
+                <li class="">得意分野が入る</li>
+                <li class="">得意分野が入る得意分野が入る</li>
+              </ul>
+            </div>
+          </div>
+          <?php include 'components/pager.php';?>
+        </div>
+      </section>
+    </main>
+    <?php include 'components/aside-travel-content.php';?>
+  </div>
+</div>
+
+<?php include 'footer.php';?>
--- newers-guide.php
+++ newers-guide.php
@@ -18,19 +18,19 @@
         <div class="head">ご旅行の流れ</div>
         <div class="text">たびチョクはお客様に合わせ、現地パートナーが提案する「旅コンテンツ」から進める方法と、たびチョク事務局または現地パートナーとメッセージを通して旅を確定していく方法がございます。<br>お客様のご希望の方法を下記よりお選びください。</div>
         <div class="row links">
-          <div class="col col-12 col-md-3 link">
+          <div class="col col-12 col-md-4 link">
             <a href="#basic">
-            いろいろ検索して、自分で旅を探してみたい！
-            <div class="button">
-              <span>旅を探す</span><br>
-              ベーシックコース
-            </div>
-            旅行契約形態：手配旅行契約
+              <span>いろいろ検索して、自分で旅を探してみたい！</spa>
+              <div class="button">
+                <span>旅を探す</span><br>
+                ベーシックコース
+              </div>
+              旅行契約形態：手配旅行契約
             </a>
           </div>
-          <div class="col col-12 col-md-3 link">
-            <a href="#stanard">
-              あれもこれもまとめて一気に、<br>現地に直接相談したい！
+          <div class="col col-12 col-md-4 link">
+            <a href="#standard">
+              <span>あれもこれもまとめて一気に、<br>現地に直接相談したい！</span>
               <div class="button">
                 <span>事業者を探す</span><br>
                 スタンダードコース
@@ -38,9 +38,9 @@
               旅行契約形態：手配旅行契約
             </a>
           </div>
-          <div class="col col-12 col-md-3 link">
+          <div class="col col-12 col-md-4 link">
             <a href="#full">
-            出発から帰国まで丸ごと<br>日本の旅行会社に相談したい！
+              <span>出発から帰国まで丸ごと<br>日本の旅行会社に相談したい！</span>
               <div class="button">
                 <span>たびチョク事務局に任せる</span><br>
                 フルコース
@@ -55,15 +55,15 @@
       <div class="inner">
         <div class="head"><span>旅を探す</span> ベーシックコース</div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>01
               </div>
-              <img src="/assets/img/newers-guides-basic01.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step01.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">まずはさがす！</div>
             <div class="text">
             現地パートナーが提案する「旅コンテンツ」から自分の好きな旅を探してください。<br>
@@ -73,15 +73,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>02
               </div>
-              <img src="/assets/img/newers-guides-basic02.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step02.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-19">
             <div class="head">空き状況・料金を聞く！</div>
             <div class="text">
             すぐに申し込みたい気持ちを抑えて、まずは「旅コンテンツ」の空き状況と料金を聞いてみよう！<br>
@@ -93,15 +93,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>03
               </div>
-              <img src="/assets/img/newers-guides-basic03.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step03.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">手配内容を承諾する！</div>
             <div class="text">
             現地から空き状況と料金が届いたら「見積書」をよく確認してください。現地通貨のものが日本円に換算されて届きます。質問や変更があったら、直接メッセージを送ってください。新しい見積書が送られてきます。<br>
@@ -113,15 +113,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>04
               </div>
-              <img src="/assets/img/newers-guides-basic04.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step04.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">いざ、申し込む！</div>
             <div class="text">
             現地からメッセージで予約が取れたという連絡と「確定見積書」が届きます。<br>
@@ -133,15 +133,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>05
               </div>
-              <img src="/assets/img/newers-guides-basic05.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step05.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-19">
             <div class="head">契約成立！</div>
             <div class="text">
             「申込」が完了しますと、メッセージで「請求書」「旅行条件書」が届きます。<br>
@@ -152,15 +152,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>06
               </div>
-              <img src="/assets/img/newers-guides-basic06.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step06.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">契約成立！</div>
             <div class="text">
             「申込」が完了しますと、メッセージで「請求書」「旅行条件書」が届きます。<br>
@@ -171,15 +171,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>07
               </div>
-              <img src="/assets/img/newers-guides-basic07.svg" alt="">
+              <img src="/assets/img/newers-guide-basic-step07.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">旅をする！</div>
             <div class="text">
             サービスを受けるまでは自己責任です。旅券、査証、航空券、保険、その国の事情など旅の準備は大丈夫ですか？<br>
@@ -194,19 +194,19 @@
         <a href="" class="button">旅コンテンツを見る</a>
       </div>
     </section>
-    <section id="" class="section-content stanard">
+    <section id="" class="section-content standard">
       <div class="inner">
       <div class="head"><span>事業者を探す</span> スタンダードコース</div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>01
               </div>
-              <img src="/assets/img/newers-guides-stanard01.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step01.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">まずはさがす！</div>
             <div class="text">
             事業者を探すから自分の行きたいエリアを決めて、まずはその地域を担当する現地パートナーを探そう！<br>
@@ -218,15 +218,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>02
               </div>
-              <img src="/assets/img/newers-guides-stanard02.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step02.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">見積・日程を聞く！</div>
             <div class="text">
             はじめてコンタクトするときは誰でも緊張します。でも、事業者はホスピタリティ産業だから親切な人が多いよ。<br>
@@ -238,15 +238,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>03
               </div>
-              <img src="/assets/img/newers-guides-stanard03.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step03.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">手配内容を承諾する！</div>
             <div class="text">
             現地から「見積書」が届いたら、よく確認してください。現地通貨のものが日本円に換算されて届きます。<br>
@@ -258,15 +258,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>04
               </div>
-              <img src="/assets/img/newers-guides-stanard04.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step04.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">いざ、申し込む！</div>
             <div class="text">
             現地からメッセージで予約が取れたという連絡と「確定見積書」が届きます。<br>
@@ -278,15 +278,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>05
               </div>
-              <img src="/assets/img/newers-guides-stanard05.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step05.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">契約成立！</div>
             <div class="text">
             「申込」が完了しますと、メッセージで「請求書」「旅行条件書」が届きます。「取消料基準」をご確認下さい！
@@ -298,15 +298,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>06
               </div>
-              <img src="/assets/img/newers-guides-stanard06.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step06.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">ファイナルが届く！</div>
             <div class="text">
             ご利用日の1週間前頃には現地から「最終確認書」がメッセージで届きます。<br>
@@ -319,15 +319,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>07
               </div>
-              <img src="/assets/img/newers-guides-stanard07.svg" alt="">
+              <img src="/assets/img/newers-guide-standard-step07.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">旅をする！</div>
             <div class="text">
             サービスを受けるまでは自己責任です。旅券、査証、航空券、保険、その国の事情など旅の準備は大丈夫ですか？<br>
@@ -343,18 +343,18 @@
       </div>
     </section>
     <section id="" class="section-content full">
-      <div class="inner">
+      <div class="inner"> 
         <div class="head"><span>たびチョク事務局に任せる</span> フルコース</div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>01
               </div>
-              <img src="/assets/img/newers-guides-full01.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step01.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">まずはお気軽に相談！</div>
             <div class="text">
             お客様のご依頼に基づいて、当社が旅行計画を企画し、航空券、地上費、企画費などを包括してお見積します。<br>
@@ -367,15 +367,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>02
               </div>
-              <img src="/assets/img/newers-guides-full02.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step02.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">見積・日程を聞く！</div>
             <div class="text">
             はじめてコンタクトするときは誰でも緊張します。でも、事業者はホスピタリティ産業だから親切な人が多いよ。<br>
@@ -387,15 +387,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>03
               </div>
-              <img src="/assets/img/newers-guides-full03.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step03.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">見積・日程を承諾する！</div>
             <div class="text">
             たびチョクから「日程表」「見積書」が届いたら、よく中身をご確認してください。<br>
@@ -407,15 +407,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>04
               </div>
-              <img src="/assets/img/newers-guides-full04.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step04.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">いざ、申し込む！</div>
             <div class="text">
             メッセージで航空券やホテルの予約が取れたという連絡と「確定見積書」が届きます。<br>
@@ -427,15 +427,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>05
               </div>
-              <img src="/assets/img/newers-guides-full05.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step05.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">契約成立！</div>
             <div class="text">
             「申込」が完了しますと、メッセージで「請求書」「旅行条件書」が届きます。「取消料基準」をご確認下さい！<br>
@@ -447,15 +447,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>06
               </div>
-              <img src="/assets/img/newers-guides-full06.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step06.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">最終日程表が届く！</div>
             <div class="text">
             残金のお支払い後、ご利用日の1週間前頃には「最終日程表」がメッセージで届きます。<br>
@@ -468,15 +468,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col col-12">
+          <div class="col col-12 col-md-3">
             <div class="img">
               <div class="step">
                 <span>STEP</span><br>07
               </div>
-              <img src="/assets/img/newers-guides-full07.svg" alt="">
+              <img src="/assets/img/newers-guide-full-step07.svg" alt="">
             </div>
           </div>
-          <div class="col col-12">
+          <div class="col col-9">
             <div class="head">旅をする！</div>
             <div class="text">
             サービスを受けるまでは自己責任です。旅券、査証、航空券、保険、その国の事情など旅の準備は大丈夫ですか？<br>
@@ -487,24 +487,24 @@
             </div>
           </div>
         </div>
-        <div class="head">まずは会員登録！</div>
-        <div class="text">会員登録と合わせてたびチョクに相談も可能です！<br>気になる方はご要望欄へご記入ください。</div>
         <div class="form">
+          <div class="head">まずは会員登録！</div>
+          <div class="text">会員登録と合わせてたびチョクに相談も可能です！<br>気になる方はご要望欄へご記入ください。</div>
           <form action="">
-              <dl>
-                <dt class="required">メールアドレス</dt>
-                <dd>
-                  <input type="text" placeholder="tabichoku@example.com">
-                </dd>
-                <dt class="required">電話番号</dt>
-                <dd>
-                  <input type="text" placeholder="0000000000">
-                </dd>
-                <dt>ご希望の旅について教えてください</dt>
-                <dd>
-                  <textarea name="" id="" cols="30" rows="10" placeholder="国、旅行ご予算、体験してみたいことなど"></textarea>
-                </dd>
-              </dl>
+            <dl>
+              <dt class="required">メールアドレス</dt>
+              <dd>
+                <input type="text" placeholder="tabichoku@example.com">
+              </dd>
+              <dt class="required">電話番号</dt>
+              <dd>
+                <input type="text" placeholder="0000000000">
+              </dd>
+              <dt>ご希望の旅について教えてください</dt>
+              <dd>
+                <textarea name="" id="" cols="30" rows="10" placeholder="国、旅行ご予算、体験してみたいことなど"></textarea>
+              </dd>
+            </dl>
           </form>
           <div class="navi">
             <ul class="links">
@@ -514,12 +514,13 @@
             </ul>
           </div>
           <div class="privacy">
-            <input type="checkbox" id="" name="" checked><label for=""><a href="">プライバシーポリシー</a>に同意します</label>
+            <input type="checkbox" class="checkbox" id="" name="" checked><label for=""><a href="">プライバシーポリシー</a>に同意します</label>
           </div>
           <div class="btn-wrap"><input class="button" type="button"  value="同意して会員登録（無料）"></div>
         </div>
       </div>
     </section>
+    <?php include 'components/category-navi.php';?>
   </main>
 </div>
 
