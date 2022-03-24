20813a92740554abe4dd4d3ac01a268f9a7ec46a
unknown-anna <unknown2011.04.art@google.com>
2022-03-24 09:58:46 -0700

    mypage template

---
+++ mypage-template.php
@@ -0,0 +1,46 @@
+<?php include 'header-mypage.php';?>
+<?php include 'breadcrumb.php';?>
+
+<script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+<script src="assets/js/mypage-profile.js" charset="UTF-8"></script>
+
+<div class="contents-wrap mypage">
+
+  <div class="main-aside-wrap">
+
+    <main class="main dashboard">
+      <div class="main-header">
+        <h2 class="main-header-title">テンプレート</h2>
+      </div>
+
+      <div class="list-template">
+
+        <div class="list-template-item">
+          <div class="list-template-item-header">ヒアリングシート</div>
+          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">テンプレート</button></a></div>
+        </div>
+
+        <div class="list-template-item">
+          <div class="list-template-item-header">日程表ひな形</div>
+          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+        </div>
+
+        <div class="list-template-item">
+          <div class="list-template-item-header">未成年申込同意書</div>
+          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+        </div>
+
+        <div class="list-template-item">
+          <div class="list-template-item-header">旅行者名簿ひな形</div>
+          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+        </div>
+      
+      </div>
+
+    </main>
+
+    <?php include 'components/aside-mypage.php';?>
+  </div>
+
+</div>
+<?php include 'footer-mypage.php';?>
