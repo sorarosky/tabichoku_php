3c9d6339c39edf47d3eba9d3e39fd1173fb60279
unknown-anna <unknown2011.04.art@google.com>
2022-03-28 18:21:10 -0700

    add page links to index and fix template page

--- index.php
+++ index.php
@@ -45,5 +45,17 @@
     <li class="link"><a href="password-mail.php">ログイン画面／新規作成 – パスワード再設定メール送信完了 password-mail.php</a></li>
     <li class="link"><a href="password-reset-form.php">ログイン画面／新規作成 – パスワード再設定 password-reset-form.php</a></li>
     <li class="link"><a href="password-reset.php">ログイン画面／新規作成 – パスワード再設定完了 password-reset.php</a></li>
+    <li class="link"><a href="dashboard.php">マイページ ダッシュボード dashboard.php</a></li>
+    <li class="link"><a href="mypage-tour-list.php">マイページ あなたの旅行情報  mypage-tour-list.php</a></li>
+    <li class="link"><a href="mypage-profile.php">マイページ 会員情報 mypage-profile.php</a></li>
+    <li class="link"><a href="mypage-template.php">マイページ テンプレート mypage-template.php</a></li>
+    <li class="link"><a href="mypage-account.php">マイページ アカウント情報 mypage-account.php</a></li>
+    <li class="link"><a href="mypage-email-edit.php">マイページ メールアドレス変更 ダッシュボード mypage-email-edit.php</a></li>
+    <li class="link"><a href="mypage-passowrd-edit.php">マイページ パスワード変更 ダッシュボード mypage-passowrd-edit.php</a></li>
+    <li class="link"><a href="mypage-favorite-tour-list.php">マイページ お気に入り 旅コンテンツ mypage-favorite-tour-list.php</a></li>
+    <li class="link"><a href="mypage-favorite-news-list.php">マイページ お気に入り 現地通信 mypage-favorite-news-list.php</a></li>
+    <li class="link"><a href="mypage-favorite-partner-list.php">マイページ お気に入り 現地パートナー mypage-favorite-partner-list.php</a></li>
+    <li class="link"><a href="mypage-tour-info-download.php">マイページ 旅行情報 重要書類 mypage-tour-info-download.php</a></li>
+
   </ul>
 </nav>
--- mypage-template.php
+++ mypage-template.php
@@ -8,32 +8,35 @@
     <?php include 'components/aside-mypage.php';?>
 
     <main class="main dashboard">
-      <div class="main-header">
-        <h2 class="main-header-title">テンプレート</h2>
-      </div>
 
-      <div class="list-template">
-
-        <div class="list-template-item">
-          <div class="list-template-item-header">ヒアリングシート</div>
-          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">テンプレート</button></a></div>
+      <div class="main-container">
+        <div class="main-header">
+          <h2 class="main-header-title">テンプレート</h2>
         </div>
 
-        <div class="list-template-item">
-          <div class="list-template-item-header">日程表ひな形</div>
-          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
-        </div>
+        <div class="list-template">
 
-        <div class="list-template-item">
-          <div class="list-template-item-header">未成年申込同意書</div>
-          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
-        </div>
+          <div class="list-template-item">
+            <div class="list-template-item-header">ヒアリングシート</div>
+            <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">テンプレート</button></a></div>
+          </div>
 
-        <div class="list-template-item">
-          <div class="list-template-item-header">旅行者名簿ひな形</div>
-          <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+          <div class="list-template-item">
+            <div class="list-template-item-header">日程表ひな形</div>
+            <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+          </div>
+
+          <div class="list-template-item">
+            <div class="list-template-item-header">未成年申込同意書</div>
+            <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+          </div>
+
+          <div class="list-template-item">
+            <div class="list-template-item-header">旅行者名簿ひな形</div>
+            <div class="list-template-item-btn"><a href=""><button class="btn btn-blue">ダウンロード</button></a></div>
+          </div>
+        
         </div>
-      
       </div>
 
     </main>
