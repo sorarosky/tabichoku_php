416e778fb56975563dbbf6ebc0ee059c9296f055
unknown-anna <unknown2011.04.art@google.com>
2022-04-28 00:37:42 -0700

    add template list

---
+++ php-data/mypage-template-data.php
@@ -0,0 +1,94 @@
+<?php
+
+$arr_template_file = array(
+
+	array(
+		'file_name' => 'ヒアリングシート（Aタイプ）.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => 'ヒアリングシート（Bタイプ）.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '和文日程表ひな形（Aタイプ）.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '和文日程表ひな形（Bタイプ）.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '和文日程表ひな形（Cタイプ）.pptx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '未成年のお客様のご旅行申込・参加同意書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '国内旅行　宿泊者名簿.xlsx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '国内旅行　団員名簿.xlsx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '国内旅行　参加申込書（お伺い書）.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '個人・団体見積手配書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '免責同意書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '変更取消用紙.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '見積仕様書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '食物アレルギーに関するアンケート.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '肖像権使用同意書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '視察申込書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '海外旅行　団員名簿.xlsx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '海外旅行　参加申込書（お伺い書）.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '海外旅行　ルーミングリスト.xlsx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => '海外旅行　パスポートリスト.xlsx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => 'チケット申込書.docx',
+		'btn_name' => 'ダウンロード',
+	),
+	array(
+		'file_name' => 'アンケート用紙.docx',
+		'btn_name' => 'ダウンロード',
+	),
+
+);
